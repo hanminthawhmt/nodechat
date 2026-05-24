987dc9b033efaf5b6a7cce9bfd64ed4de19b57dd
hanminthawhmt <hanminthaw15@gmail.com>
2026-05-24 23:39:26 +0700

    feat: add environment variables for session and Google OAuth configuration

--- src/config/env.js
+++ src/config/env.js
@@ -6,4 +6,7 @@ module.exports = {
   SALT: process.env.SALT,
   JWT_SECRET: process.env.JWT_SECRET,
   EXPIRES_IN: process.env.EXPIRES_IN,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 };
